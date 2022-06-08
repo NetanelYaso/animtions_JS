0335914b9774b10bb7a2eca40302f90d4dce27e5
NetanelYaso <netanelyaso12@gmail.com>
2022-06-08 19:43:25 +0300

    anime-1

---
+++ main.js
@@ -0,0 +1,42 @@
+// var height = 0;
+// var intervalId;
+// function playFunc(){
+//    intervalId = setInterval(()=>{
+//         div_1.style.height = height++ + "px";
+//     },10)
+
+//    }
+//   function stopFunc(){
+//     clearInterval(intervalId);
+//   }
+
+//   function stopWithEnter(e){
+//    if(e.key == "Enter"){
+//        stopFunc();
+//     }
+// }
+
+// function pressMe(){
+//    var color1 = Math.floor(Math.random()*256);
+//    var color2 = Math.floor(Math.random()*256);
+//    var color3 = Math.floor(Math.random()*256);
+//    div_2.style.backgroundColor = "rgb("+color1+","+color2+","+color3+")";
+// }
+
+// var dictance = 0;
+// var intervalId;
+// function getHeightWidth(){
+// intervalId = setInterval(()=>{
+//     if(height_input.value > dictance){
+//         div_3.style.height = dictance++ + "px";
+//     }
+//     if(width_input.value > dictance){
+//        div_3.style.width = dictance++ + "px";
+//     }50
+// },10)
+// }
+
+// function stopSpace() {
+//     clearInterval(intervalId);
+// }
+
